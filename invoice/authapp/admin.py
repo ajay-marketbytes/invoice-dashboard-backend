c194b3a1c74df5892fc0ba0478dab245a0fe26ad
ajay-marketbytes <ajay@marketbytes.in>
2025-03-14 22:46:42 +0530

    .

--- invoice/authapp/admin.py
+++ invoice/authapp/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'first_name', 'last_name', 'is_active', 'is_staff')}
+        ),
+    )
+
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'username', 'password')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name')
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+    readonly_fields = ('last_login', 'date_joined')
+
+admin.site.register(CustomUser, CustomUserAdmin)
