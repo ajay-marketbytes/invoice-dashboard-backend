3d09ca749095e17a6fe017418b6e57abb87c6be7
ajay-marketbytes <ajay@marketbytes.in>
2025-04-02 01:02:03 +0530

    .

--- invoice_backend/branch/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Branch
-
-class BranchSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Branch
-        fields = ['id', 'branch_address', 'state', 'city', 'gstin', 'phone_code', 'phone', 'website', 'series_prefix', 'last_invoice_number', 'last_reset_date']
--- invoice_backend/clients/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ClientListCreateView, ClientDetailView
- 
-urlpatterns = [
-    path('clients/', ClientListCreateView.as_view(), name='client-list-create'),
-    path('clients/<int:pk>/', ClientDetailView.as_view(), name='client-detail'),
-]
